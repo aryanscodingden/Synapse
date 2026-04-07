8c22f0cb4deb41f36af5fea40eb17246b2357475
Aryan Rao <aryanscode@gmail.com>
2026-04-07 14:34:28 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <h1 className='text-2xl font-bold'>Synapse</h1>
+    </div>
+  )
+}
